d027e71d00d37cf23a9b5d1797f67bf2b09202cf
cruse07 <cruse07@gmail.com>
2020-02-23 23:35:20 +0530

    added virtual list

--- src/app/virtuallist/virtuallist.component.ts
+++ src/app/virtuallist/virtuallist.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit, Input, OnChanges, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { ChangeEvent } from 'angular-virtual-list';
+import { CompileShallowModuleMetadata } from '@angular/compiler';
 
 
 export class UserModel {
   id: number;
   name?: string;
-  age?: number;
+  contact?: string;
+  address?:string;
 }
 
 @Component({
@@ -14,56 +16,39 @@ export class UserModel {
   templateUrl: './virtuallist.component.html',
   styleUrls: ['./virtuallist.component.scss']
 })
-export class VirtuallistComponent implements OnInit, OnChanges {
-  ngOnChanges(changes: import("@angular/core").SimpleChanges): void {
-    throw new Error("Method not implemented.");
-  }
-  
-  @ViewChild('theader') head:ElementRef;
-
-  @Input()
+export class VirtuallistComponent implements OnInit {
   public items: Array<UserModel>=[];
   public scrollList: Array<UserModel>=[];
   protected items$ = new BehaviorSubject<UserModel[]>(null);
   protected buffer: UserModel[] = [];
   protected loading: boolean;
+  startingItem:number;
+  enditem:number;
   constructor() {
     this.setItem();
   }
 
   ngOnInit() {
   }
-  // ngAfterContentInit(){
-  //   var itemPerView = (this.head.nativeElement.scrollHeight/20);
-  //   this.scrollList= this.items.splice(0, itemPerView);
-  //   console.log(this.scrollList);
-  // }
   protected fetchMore(event: ChangeEvent) {
-    if (event.end !== this.buffer.length) return;
-    this.loading = true;
-    // this.fetchNextChunk(this.buffer.length, 5).then(chunk => {
-    //   this.buffer = this.buffer.concat(chunk);
-    //   this.items$.next(this.buffer);
-    //   this.loading = false;
-    // }, () => this.loading = false);
-  }
+    this.startingItem=event.start+1;
+    this.enditem= event.end;
+    console.log(event.start);
+    console.log(event.end);
+    if (event.end == this.buffer.length) 
+    return;
+    
+}
 
 
-  protected fetchNextChunk(skip: number, limit: number): Promise<UserModel[]> {
-    return new Promise((resolve, reject) => {
-
-    });
-  }
-  
   public setItem() {
-    for (var i = 0; i <= 100000; i++) {
+    for (var i = 0; i <1000000; i++) {
       var item = new UserModel()
       item.id = i+1;
       item.name = "Name " + (i+1).toString();
-      item.age=1.5*(i+1);
+      item.contact="+1 650-253-0000";
+      item.address=(1600+i+1).toString()+" Amphitheatre Pkwy, Mountain View, CA 94043, United States";
       this.items.push(item);
-     // this.scrollList.push(item)
-      // this.buffer.push(item)
     }
     this.items$.next(this.items)
     console.log(this.scrollList)
